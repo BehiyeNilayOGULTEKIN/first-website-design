ba518dee4263c4d03e34569c1faabbd81a755555
BehiyeNilayOGULTEKIN <behiyenilayogultekin@gmail.com>
2023-06-23 16:26:30 +0300

    updated
    -ejs dosyaları tekrar düzenlendi
    -readme dosyasında minik sprint bilgilendirmesi eklendi

--- Readme.txt
+++ Readme.txt
@@ -7,4 +7,23 @@ I used bootstrap 4.6 version for downlond it,check their website
 also you can see many code documentations in their website
 check this as my design source:https://getbootstrap.com/docs/4.0/getting-started/introduction/
 also check this video for my visual stduio exc:https://www.youtube.com/watch?v=Cb3PjfD90wQ*/
-
+We used Mongo db as a server,
+and also we used Netfily for taking a hosting services
+My Sprint for this project:
+sprint 1:defining the project and determining the requirements(100 p)(for 1 week)
+updated(2 week)+
+sprint 2 :Start to coding front end site(300 p)
+-last movies site+
+-traliers page+
+-traliers info page+
+(for 1 week)
+sprint 3: Start to coding back end site
+-pulling traliers titles and img urls from the database+
+-pulling comments from the database
+-pulling necessary datas for the page-traliers-link-index
+-viewing the comments
+-adding new movie to the page(for admin page)
+-removing movie to the page(for admin page)
+-updating the movie (for admin page)
+(for one week)(700 points)
+updated(+ 2 weeks more)
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express =require('express');
 const mongoose=require('mongoose');
 const app =express();
 const ejs =require('ejs');
-
+// const bodyParser = require('body-parser');
 app.set('view engine','ejs');
  mongoose.connect('mongodb+srv://Admin:1.nilayunutma@mydatabase.ilwyvfv.mongodb.net/beyazperde?retryWrites=true&w=majority');
 //  const movieSchema={
@@ -39,26 +39,122 @@ app.set('view engine','ejs');
 //  app.listen(4000,function() {
 //     console.log("server is running");
 //  })
-const movieSchema = {
-   title: String,
-   posterurl: String,
- };
- //Defining the variables
- const Movie = mongoose.model('Movie', movieSchema);
- //calling mongodb 
- app.get('/', (req, res) => {
-   Movie.find({}).limit(6)
-     .then(movies => {
-       res.render('page-traliers-index', {
-         moviesList: movies,
-       });
-     })
-     .catch(err => {
-      console.log("Error!");
-     });
- });
- app.use('/public', express.static('public'));
- app.listen(4000, function () {
-   console.log("server is running");
- });
- //run the server in localhost-4000
+// const movieSchema = new mongoose.Schema({
+//   title: String,
+//   posterurl: String,
+// });
+
+// const movieSchema2 = new mongoose.Schema({
+//   posterurl: String,
+//   trailer: String,
+//   watch: String,
+// });
+
+// const Movie = mongoose.model('Movie', movieSchema);
+// const Movie2 = mongoose.model('Movie2', movieSchema2);
+
+// app.use('/public', express.static('public'));
+
+// app.get('/', (req, res) => {
+//   Movie.find({}).limit(6)
+//     .then(movies => {
+//       res.render('page-trailers-index', {
+//         moviesList: movies,
+//       });
+//     })
+//     .catch(err => {
+//       console.log("Error!");
+//     });
+// });
+
+// app.get('/recent', (req, res) => {
+//   Movie2.find({}).limit(6)
+//     .then(recentMovies => {
+//       res.render('page-trailers-index', {
+//         moviesList2: recentMovies,
+//       });
+//     })
+//     .catch(err => {
+//       console.log("Error2!");
+//     });
+// });
+
+// app.listen(4000, function () {
+//   console.log("Server is running");
+// });
+const movieSchema = new mongoose.Schema({
+  title: String,
+  posterurl: String,
+});
+
+const movieSchema2 = new mongoose.Schema({
+  posterurl: String,
+  trailer: String,
+  watch: String,
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const Movie2 = mongoose.model('Movie2', movieSchema2);
+
+app.use('/public', express.static('public'));
+// app.use(bodyParser.urlencoded({extended: true}));
+app.get('/', (req, res) => {
+  const moviesPromise = Movie.find({}).limit(6).exec();
+  const recentmoviesPromise = Movie2.find({}).limit(6).exec();
+
+  Promise.all([moviesPromise, recentmoviesPromise])
+    .then(([movies, recentmovies]) => {
+      res.render("page-traliers-index", {
+        moviesList: movies,
+        moviesList2: recentmovies,
+      });
+    })
+    .catch(err => {
+      console.log("Error!", err);
+      res.status(500).send("An error occurred");
+    });
+});
+app.get('/page-traliers-link-index', (req, res) => {
+
+  res.render('page-traliers-link-index');
+  
+  });
+  
+app.listen(4000, () => {
+  console.log("Server is running");
+});
+app.get('/actors-page', (req, res) => {
+
+    res.render('actors-page');
+    
+    });
+app.get('/page-traliers-link-index', (req, res) => {
+
+    res.render('page-traliers-link-index');
+      
+    });
+app.get('/comments_sec', (req, res) => {
+
+        res.render('comments_sec');
+          
+    });
+app.get('/main_movie_tralier_page', (req, res) => {
+
+            res.render('main_movie_tralier_page');
+              
+    });
+app.get('/renewed-tv-series', (req, res) => {
+
+      res.render('renewed-tv-series');
+        
+});
+app.get('/seasons-eposides', (req, res) => {
+
+  res.render('seasons-eposides');
+    
+});
+app.get('/serie-tralier-index', (req, res) => {
+
+  res.render('serie-tralier-index');
+    
+});
